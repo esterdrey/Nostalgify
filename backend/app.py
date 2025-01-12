8f8f52d10dbc0d72b63e9fa940a933cd17f9de7a
Naama Leah Radonsky <radonsky@acad.jct.ac.il>
2025-01-12 17:27:11 +0200

    חוזר

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,87 @@
-from flask import Flask, render_template
+import base64
+import os
+import torch
+import requests
+from torchvision import transforms
+from flask import Flask, render_template, send_from_directory, request, jsonify
+from io import BytesIO
+from PIL import Image
 
-# יצירת אובייקט Flask
+# הגדרת Flask
 app = Flask(__name__, static_folder='../frontend', template_folder='../frontend')
 
-# רוט להצגת עמוד הבית
+def predict_age(image_path):
+    url = "https://api.deepai.org/api/demographic-recognition"
+    with open(image_path, 'rb') as image_file:
+        response = requests.post(
+            url,
+            files={'image': image_file},
+            headers={'api-key': '6e1b8fec-b405-41c6-813e-6a820e93f9f5'}
+        )
+    result = response.json()
+    try:
+        age = result['output']['faces'][0]['age']
+        return age
+    except (KeyError, IndexError):
+        return "Error: Unable to detect age."
+
+
 @app.route('/')
 def home():
+    """ הצגת עמוד הבית - index.html"""
     return render_template('index.html')
 
-# הפעלת השרת
+@app.route('/process', methods=['POST'])
+def process_image():
+ 
+    try:
+        # קבלת הנתונים מה-Frontend
+        data = request.json
+        if not data or 'image' not in data or 'country' not in data:
+            return jsonify({"error": "Missing 'image' or 'country' in request"}), 400
+
+        image_data = data['image']
+        country = data['country']
+
+        # והפרדת HEADER המרת התמונה לבינארי
+        header, encoded = image_data.split(",", 1)
+        image_binary = base64.b64decode(encoded)
+
+        #יצירת אובייקט תמונה 
+        image = Image.open(BytesIO(image_binary))
+
+        # שמירת התמונה לקובץ זמני)
+        temp_image_path = "temp_image.jpg"
+        image.save(temp_image_path)
+
+        # ניתוח גיל 
+        age = predict_age(temp_image_path)
+
+        # ניקוי קובץ זמני
+        os.remove(temp_image_path)
+
+        # החזרת הגיל כמספר בלבד
+        #return str(age)
+   
+
+        # יצירת לינק לפלייליסט מותאם
+        playlist_link = f"https://open.spotify.com/playlist/dummy_playlist_for_{country}_facecount_{age}"
+        return jsonify({"playlist": playlist_link, "age": age})
+
+  
+    except Exception as e:
+        return jsonify({"error": f"An error occurred : {str(e)}"}), 500
+
+@app.route('/frontend/<path:filename>')
+def serve_static_files(filename):
+    return send_from_directory('../frontend', filename)
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host="0.0.0.0", port=5000) 
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=True, host="0.0.0.0", port=port)
